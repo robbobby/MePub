6791d28dbbad023f2b63be99b1e9607f547848a0
Rob <rob@Robs-iMac.local>
2020-10-22 19:19:46 +0100

    All tests working for initial 3 classes

--- src/customer.py
+++ src/customer.py
@@ -1,7 +1,12 @@
 class Customer:
-    def __init__(self, name, age, hunger, thirst, drunk_level):
+    def __init__(self, name, age, hunger, thirst, drunk_level, naughty_level):
+        self.naughty_level = naughty_level
         self.drunk_level = drunk_level
         self.thirst = thirst
         self.hunger = hunger
         self.name = name
         self.age = age
+
+    def is_over_18(self):
+        return self.age >= 18
+
--- tests/customer_tests.py
+++ tests/customer_tests.py
@@ -2,4 +2,35 @@ import unittest
 from src.customer import Customer
 
 class TestCustomer(unittest.TestCase):
-    pass
+    def setUp(self):
+        self.customer = Customer("customer name", 17, 3, 4, 5, 100)
+        self.customer2 = Customer("customer name", 18, 3, 4, 5, 100)
+        self.customer3 = Customer("customer name", 40, 3, 4, 5, 100)
+            ## name, age, hunger, thirst, drunk_level
+
+    def test_customer_has_name(self):
+        self.assertEqual("customer name", self.customer.name)
+
+    def test_customer_has_age(self):
+        self.assertEqual(17, self.customer.age)
+
+    def test_customer_has_hunger(self):
+        self.assertEqual(3, self.customer.hunger)
+
+    def test_customer_has_thirst(self):
+        self.assertEqual(4,self.customer.thirst)
+
+    def test_customer_has_drunk_level(self):
+        self.assertEqual(5, self.customer.drunk_level)
+
+    def test_customer_has_naughty_level(self):
+        self.assertEqual(100,self.customer.naughty_level)
+
+    def test_customer_can_drink_alchohol(self):
+        self.assertEqual(False, self.customer.is_over_18())
+
+    def test_customer__can_drink_alchohol(self):
+        self.assertEqual(True, self.customer2.is_over_18())
+
+    def test_customer___can_drink_alchohol(self):
+        self.assertEqual(True, self.customer3.is_over_18())
